88620c87a69f06e94d268c127185f8cb0eb4e180
yves <yves@lepidus.com.br>
2023-04-18 16:57:39 -0400

    Refactors tests.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#550
    
    Signed-off-by: yves <yves@lepidus.com.br>
    Signed-off-by: iudi <iudi@lepidus.com.br>

--- cypress/tests/Test3_ValidateSitePlugin.spec.js
+++ cypress/tests/Test3_ValidateSitePlugin.spec.js
@@ -1,28 +1,7 @@
 const admin = Cypress.env('adminUser');
 const adminPassword = Cypress.env('adminPassword');
 
-describe('Test with many contexts', function() {
-	it('Creates a context', function() {
-		cy.login(admin, adminPassword);
-        cy.get('a:contains(' + admin + '):visible').click();
-        cy.get('a:contains("Dashboard"):visible').click();
-        cy.get('.app__nav a').contains('Administration').click();
-        cy.get(':nth-child(2) > :nth-child(1) > a').click();
-		cy.get('div[id=contextGridContainer]').find('a').contains('Create').click();
-
-		cy.wait(1000);
-		cy.get('input[name="name-en_US"]').type('journal2', {delay: 0});
-		cy.get('input[name=acronym-en_US]').type('JPK', {delay: 0});
-		cy.get('span').contains('Enable').siblings('input').check();
-		cy.get('input[name="supportedLocales"][value="en_US').check();
-		cy.get('input[name="primaryLocale"][value="en_US').check();
-
-		cy.get('input[name=urlPath]').type('publicknowledge2', {delay: 0});
-
-		cy.get('button').contains('Save').click();
-        cy.wait(1000);
-	});
-
+describe('Validates site plugin', function() {
     it('Enable plugin in site settings', function() {
 		cy.login(admin, adminPassword);
         cy.get('a:contains("journal2"):visible').click();
