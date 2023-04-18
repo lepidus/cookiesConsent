c808e581488af51aa1c47e2f6cf91abd955a96ca
yves <yves@lepidus.com.br>
2023-04-18 14:59:24 -0400

    Add cypress integration test and import ci model.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#550
    
    Signed-off-by: yves <yves@lepidus.com.br>
    Signed-off-by: iudi <iudi@lepidus.com.br>

---
+++ cypress/tests/Test1_ValidateJournalPlugin.spec.js
@@ -0,0 +1,38 @@
+const admin = Cypress.env('adminUser');
+const adminPassword = Cypress.env('adminPassword');
+const context = Cypress.env('context');
+
+describe('Starts and View the block plugin', function() {
+  it('Activate Plugin', function() {
+    cy.login(admin, adminPassword, context);
+    cy.get('.app__nav a')
+      .contains('Website')
+      .click();
+    cy.get('button[id="plugins-button"]').click();
+    cy.get('body').then(($body) => {
+      if (
+        !(
+          $body.find(
+            'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-cookiesconsentplugin"] > :nth-child(3) > :nth-child(1) > :checked'
+          ).length > 0
+        )
+      ) {
+        cy.get(
+          '#component-grid-settings-plugins-settingsplugingrid-category-generic-row-cookiesconsentplugin > :nth-child(3) >'
+        ).click();
+        cy.get(
+          'div:contains(\'The plugin "Cookies Consent" has been enabled.\')'
+        );
+      }
+    });
+    cy.logout();
+  });
+
+  it('Show the keyword cloud plugin in homepage', function() {
+    cy.login(admin, adminPassword, context);
+    cy.get('.app__contextTitle').click();
+    cy.get('.cc-window').should('be.visible');
+    cy.get('.cc-window').contains('This website uses cookies to ensure your best experience Privacy Statement');
+    cy.get('.cc-btn').should('be.visible');
+  });
+});
