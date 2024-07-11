7373cf1758250caefd9dad6e2e345888c967fabe
yves <yves@lepidus.com.br>
2024-07-11 17:30:36 -0400

    Fixes Cypress tests.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#848
    
    Signed-off-by: yves <yves@lepidus.com.br>

--- cypress/tests/Test2_CreatesManyContexts.spec.js
+++ cypress/tests/Test2_CreatesManyContexts.spec.js
@@ -7,15 +7,19 @@ describe('Creates many contexts', function() {
         cy.get('a:contains(' + admin + '):visible').click();
         cy.get('a:contains("Dashboard"):visible').click();
         cy.get('.app__nav a').contains('Administration').click();
-        cy.get(':nth-child(2) > :nth-child(1) > a').click();
+        cy.get('.pkpButton').contains('Hosted Journals').click();
 		cy.get('div[id=contextGridContainer]').find('a').contains('Create').click();
 
 		cy.wait(1000);
-		cy.get('input[name="name-en_US"]').type('journal2', {delay: 0});
-		cy.get('input[name=acronym-en_US]').type('JPK', {delay: 0});
+		cy.get('#context-name-control-en').type('journal2', {delay: 0});
+		cy.get('#context-acronym-control-en').type('JPK', {delay: 0});
+		cy.get('#context-contactName-control').type('Dummy Publisher', {delay: 0});
+		cy.get('#context-contactEmail-control').type('DummyPublisher@mailinator.com', {delay: 0});
+		cy.get('select[id=context-country-control]').select('Brazil');
+		cy.get('#context-urlPath-control').type('tests', {delay: 0});
 		cy.get('span').contains('Enable').siblings('input').check();
-		cy.get('input[name="supportedLocales"][value="en_US"]').check();
-		cy.get('input[name="primaryLocale"][value="en_US"]').check();
+		cy.get('input[name="supportedLocales"][value="en"]').check();
+		cy.get('input[name="primaryLocale"][value="en"]').check();
 
 
 		cy.get('input[name=urlPath]').type('publicknowledge2', {delay: 0});
